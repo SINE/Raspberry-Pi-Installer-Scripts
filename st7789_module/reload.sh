c1a6c0d5510d8fbe88d1a098cb3d724a723e86d3
SINE <11297260+SINE@users.noreply.github.com>
2026-05-02 06:44:05 +0200

    Refactor reload script to streamline compilation and loading of kernel modules

--- st7789_module/reload.sh
+++ st7789_module/reload.sh
@@ -5,10 +5,10 @@ sudo dtoverlay -r drm-minipitft114
 sudo modprobe -r fb_st7789v
 sudo modprobe -r fbtft
 
-# Compile
-sudo make
+# Compile and install into kernel module tree
+sudo make install
 
 # Load again
 sudo modprobe fbtft
-sudo insmod ./fb_st7789v.ko
+sudo modprobe fb_st7789v
 sudo dtoverlay drm-minipitft114
